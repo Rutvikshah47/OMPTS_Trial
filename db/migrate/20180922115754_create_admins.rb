c3c79fd6d3c5e7c6564535ec76283da596fcfe60
Rutvik <rutvikshah47@gmail.com>
2018-09-27 14:28:03 +0530

    Update 20180922115754_create_admins.rb

--- db/migrate/20180922115754_create_admins.rb
+++ db/migrate/20180922115754_create_admins.rb
@@ -4,7 +4,8 @@ class CreateAdmins < ActiveRecord::Migration[5.2]
       t.string :area
       t.string :email
       t.string :password_digest
-
+      t.string :contactno
+      t.string :name
       t.timestamps
     end
   end
